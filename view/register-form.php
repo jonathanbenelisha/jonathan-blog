9bd7c101da08c588fcab32b315b98e79de5ff0aa
jonathan benelisha <jonathanbenelisha@gmail.com>
2014-12-02 12:15:37 -0800

    video 37 added new file making it so so only users can access posts

---
+++ view/register-form.php
@@ -0,0 +1,16 @@
+<form>
+<div>
+	<label for="Email"> : </label>
+	<input type="text" name="email" />
+</div>
+	<label for="username">Username: </label>
+	<input type="text" name="username" />
+<div>
+	<label for="Password">Passwword: </label>
+	<input type="password" name="password" />
+</div>
+
+<div>
+	<button type="submit">Submit</button>
+</div>
+</form>
